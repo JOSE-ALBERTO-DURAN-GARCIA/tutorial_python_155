01e445ca4ac93c36cd04066eb98e54cdbb4fb15c
jose Duran Garcia <jose9708561@gmail.com>
2023-11-20 13:12:18 -0400

    variables y operadores

---
+++ fundamentos/operadores_basicos.py
@@ -0,0 +1,38 @@
+""" 
+Operadores Aritmeticos
+"""
+
+numero1 = float (input('Ingresa un numero '))
+numero2 = float(input('Ingresa un numero '))
+
+
+#print(f'Suma {numero1 + numero2}')
+#print(f'Resta {numero1 - numero2}')
+#print(f'Multiplicacion {numero1 * numero2}')
+#print(f'Division {numero1 / numero2}')
+
+#print(f'modulo {numero1 % numero2}')
+#print(f'potencia {numero1 ** numero2}')
+#print(f'division exacta {numero1 // numero2}')
+
+
+"""
+Operadores Relacionales
+"""
+#print(" > ",numero1 > numero2)
+#print(" < ",numero1 < numero2)
+#print(" >= ",numero1 >= numero2)
+#print(" <= ",numero1 <= numero2)
+#print(" == ",numero1 == numero2)
+#print(" != ",numero1 != numero2)
+
+"""
+Operadores de asignacion
+"""
+numero3 = 8
+numero3 =numero3 - 2
+print(numero3)
+
+"""Operadores logicos"""
+
+# and, or, not
---
+++ fundamentos/variables_tipos.py
@@ -0,0 +1,22 @@
+#es un contenedor que nos permite almacenar informacion
+
+
+#metodo de salida porque muestra un resultado
+#              print('hola mundo')
+
+#metodo de entrada
+#              input('Digita una letra')
+
+
+#numero1 = 9
+#numero2 = 8
+
+#resultado = numero1+numero2
+
+#print(resultado)
+
+numero1= float(input('Ingresa un numero '))
+numero2= float(input('Ingresa un numero '))
+
+resultado = numero1+numero2
+print(resultado)
